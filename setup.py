61218487343001e1a7c21578cf51477d903a6820
Adrian <Adrian>
2022-03-02 14:49:06 +0100

    point to public repo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='mbdata',
       version='0.1',
       description='metabase api query package',
-      url='https://github.com/scale-vector/metabase_python',
+      url='https://github.com/scale-vector/metabase_data_api',
       author='scalevector',
       author_email='adrian@scalevector.ai, marcin@scalevector.ai, matthaus@scalevector.ai,',
       license='MIT',
